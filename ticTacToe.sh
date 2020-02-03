3ce646f2809840b6177065c903811c698a092859
kn52 <ashish52922@gmail.com>
2020-02-03 10:48:31 +0530

    ADDED:Function to reset the game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+readonly ROWS=3
+readonly COLUMNS=3
+declare -A board
+
 echo "Welcome to Tic Tac Toe Problem"
 
+resetGame()
+{
+	for ((i=0;i<$ROWS;i++))
+	do
+		for ((j=0;j<$COLUMNS;j++))
+		do
+			board[$i,$j]=""
+		done
+	done
+}
 
+resetGame
